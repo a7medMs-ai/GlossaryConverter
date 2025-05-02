ef3a9aaa40a17be258300a87fb6db458fb90d053
a7medMs-ai <ahmed.mostafsaad@gmail.com>
2025-05-02 16:38:50 +0300

    Update sdltm_to_tmx.py

--- utils/sdltm_to_tmx.py
+++ utils/sdltm_to_tmx.py
@@ -1,6 +1,6 @@
 import os
-from GlossaryConverter.parsers.sdltm_parser import extract_sdltm_data
-from GlossaryConverter.parsers.tmx_writer import write_tmx
+from parsers.sdltm_parser import extract_sdltm_data
+from parsers.tmx_writer import write_tmx
 
 def convert_sdltm_to_tmx(input_file, output_file=None):
     if not os.path.exists(input_file):
