fcca773ee5eccb7c83d25e667d8db66f858a480f
a7medMs-ai <ahmed.mostafsaad@gmail.com>
2025-05-02 16:32:58 +0300

    Update sdltb_to_tbx.py

--- utils/sdltb_to_tbx.py
+++ utils/sdltb_to_tbx.py
@@ -11,6 +11,6 @@ def convert_sdltb_to_tbx(sdltb_path, output_path=None):
     if output_path is None:
         output_path = os.path.splitext(sdltb_path)[0] + ".tbx"
 
-    output = run_jar("sdltb_converter.jar", [sdltb_path, output_path])
+    output = run_jar("jackcess-3.0.1.jar", [sdltb_path, output_path])
     print("[JAVA OUTPUT]:", output)
     return output_path
